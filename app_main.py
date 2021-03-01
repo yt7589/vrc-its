c0df900e2d472f1968e15bacc03b6ccac3f79592
unknown <yt7589@qq.com>
2021-03-01 21:26:02 +0800

    translate to chinese

--- app_main.py
+++ app_main.py
@@ -1,12 +1,14 @@
 import os, sys
+import time
 
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
     sys.path.append(tools)
 else:
-    sys.exit("please declare environment variable 'SUMO_HOME'")
+    sys.exit("请设置环境变量：SUMO_HOME")
 
-sumoBinary = "d:/software/Sumo/bin/sumo-gui.exe"
+sumoBinary = "d:/software/Sumo/bin/sumo-gui.exe" # 启动图形界面
+#sumoBinary = 'd:/software/Sumo/bin/sumo.exe'
 sumoCmd = [sumoBinary, "-c", "D:/software/Sumo/doc/examples/sumo/simple_nets/cross/cross3ltl/test.sumocfg"]
 
 import traci
@@ -14,9 +16,11 @@ traci.start(sumoCmd)
 step = 0
 while step < 1000:
    traci.simulationStep()
-   print('...')
+   time.sleep(1.0)
+   stepTime = traci.simulation.getTime()
+   vehicle_ids = traci.vehicle.getIDList()
+   print('{0}: {1};'.format(stepTime, len(vehicle_ids)))
    #if traci.inductionloop.getLastStepVehicleNumber("0") > 0:
    #    traci.trafficlight.setRedYellowGreenState("0", "GrGr")
    step += 1
-
 traci.close()
